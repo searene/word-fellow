ce6f28afd959638e3863d1ca1f79b904fc7562c7
Joey <mycityofsky@gmail.com>
2022-03-15 09:37:06 +0800

    Add word status combo box

--- vocab_builder/ui/DocumentDialog.py
+++ vocab_builder/ui/DocumentDialog.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QComboBox
 
 
 class DocumentDialog(QDialog):
@@ -9,9 +9,36 @@ class DocumentDialog(QDialog):
         self.document_name = document_name
         self.__init_ui()
 
-    def __init_ui(self):
-        self.setWindowTitle(self.document_name)
-
     def show_dialog(self):
         self.show()
         self.exec_()
+
+    def __init_ui(self):
+        self.setWindowTitle(self.document_name)
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(self.__get_top_bar())
+        vbox.addLayout(self.__get_middle_area())
+        vbox.addLayout(self.__get_bottom_bar())
+        self.setLayout(vbox)
+
+    def __get_top_bar(self) -> QHBoxLayout:
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.__get_status_combo_box())
+        return hbox
+
+    def __get_status_combo_box(self) -> QComboBox:
+        status_combo_box = QComboBox()
+        # TODO Fetch the list dynamically
+        status_combo_box.addItem("Unknown")
+        status_combo_box.addItem("Known")
+        status_combo_box.addItem("Studying")
+        return status_combo_box
+
+    def __get_middle_area(self) -> QHBoxLayout:
+        # TODO
+        return QHBoxLayout()
+
+    def __get_bottom_bar(self) -> QHBoxLayout:
+        # TODO
+        return QHBoxLayout()
