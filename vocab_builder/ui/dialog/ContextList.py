e7e3b64e993f404d2c81b94452acaf3a61a009e4
Joey <mycityofsky@gmail.com>
2022-04-24 18:12:38 +0800

    add update_status method

--- vocab_builder/ui/dialog/ContextList.py
+++ vocab_builder/ui/dialog/ContextList.py
@@ -1,3 +1,5 @@
+from typing import Dict, Optional
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QVBoxLayout, QLabel, QHBoxLayout, QPushButton
 
@@ -14,16 +16,28 @@ class ContextList(QtWidgets.QWidget):
 
     def __init__(self, doc: Document, status: WordStatus, db: VocabBuilderDB):
         super(ContextList, self).__init__()
+        self.__doc = doc
+        self.__status = status
         self.__db = db
-
-        # FIXME pass offset
-        self.__word = doc.get_next_word(0, status, db)
+        self.__status_to_offset_dict = {}
+        self.__word = self.__get_word(doc, status, self.__status_to_offset_dict, db)
         self.__init_ui(self.__word, doc)
 
     def update_status(self, status: WordStatus):
-        pass
+        self.__word = self.__get_word(self.__doc, status, self.__status_to_offset_dict, self.__db)
+        self.__init_ui(self.__word, self.__doc)
 
-    def __init_ui(self, word: Word, doc: Document):
+    def __get_word(self, doc: Document, status: WordStatus, status_to_offset_dict: Dict[WordStatus, int],
+                   db: VocabBuilderDB) -> Optional[str]:
+        offset = self.__get_offset(status, status_to_offset_dict)
+        return doc.get_next_word(offset, status, db)
+
+    def __get_offset(self, status: WordStatus, status_to_offset_dict: Dict[WordStatus, int]) -> int:
+        if status not in status_to_offset_dict:
+            status_to_offset_dict[status] = 0
+        return status_to_offset_dict[status]
+
+    def __init_ui(self, word: Optional[Word], doc: Document):
         vbox = QVBoxLayout()
         if word is None:
             vbox.addWidget(QLabel("No word is available"))
@@ -48,4 +62,3 @@ class ContextList(QtWidgets.QWidget):
     def __show_long_context_dialog(self, long_context: WordContext) -> None:
         long_context_dialog = LongContextDialog(long_context)
         long_context_dialog.show_dialog()
-
