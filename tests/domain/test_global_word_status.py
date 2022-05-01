c24eb76511ecfdf17af6c7bf5482cee67f630962
Joey <mycityofsky@gmail.com>
2022-05-01 14:05:23 +0800

    pass test_we_can_call_upsert_with_the_same_word_twice

--- tests/domain/test_global_word_status.py
+++ tests/domain/test_global_word_status.py
@@ -1,7 +1,8 @@
 import unittest
 
 from tests.utils import get_test_vocab_builder_db
-from vocab_builder.domain.status.GlobalWordStatus import GlobalWordStatus, insert_word_status, Status
+from vocab_builder.domain.status.GlobalWordStatus import GlobalWordStatus, insert_word_status, Status, \
+    upsert_word_status
 
 
 class GlobalWordStatusTestCase(unittest.TestCase):
@@ -9,11 +10,14 @@ class GlobalWordStatusTestCase(unittest.TestCase):
         self.__db = get_test_vocab_builder_db()
         GlobalWordStatus.init_database(self.__db)
 
-    # TODO test we can call upsert twice on the same word
     def test_word_should_be_unique(self):
         insert_word_status("test", Status.KNOWN, self.__db)
         with self.assertRaises(Exception):
             insert_word_status("test", Status.KNOWN, self.__db)
+
+    def test_we_can_call_upsert_with_the_same_word_twice(self):
+        upsert_word_status("test", Status.KNOWN, self.__db)
+        upsert_word_status("test", Status.IGNORED, self.__db)
 
 
 if __name__ == '__main__':
