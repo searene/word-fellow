73790688482e9bd1b37b4513988622c515af8bd2
Joey <mycityofsky@gmail.com>
2022-05-01 15:42:50 +0800

    move the db path out of the anki folder

--- tests/infrastructure/test_infrastructure.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class InfrastructureTestCase(unittest.TestCase):
-    def test_create_table_if_not_exists(self):
-        pass
-
-
-if __name__ == '__main__':
-    unittest.main()
--- vocab_builder/infrastructure/__init__.py
+++ vocab_builder/infrastructure/__init__.py
@@ -3,9 +3,14 @@ import os.path
 from vocab_builder.infrastructure.VocabBuilderDB import VocabBuilderDB
 
 
-def get_db_path(addon_path: str) -> str:
-    return os.path.join(addon_path, 'vocab.db')
+def get_db_path() -> str:
+    # TODO consider supporting auto-backup and suggesting restore when there's no data
+    home_dir = os.path.expanduser("~")
+    config_folder = os.path.join(home_dir, ".anki-vocab-builder")
+    if not os.path.exists(config_folder):
+        os.makedirs(config_folder)
+    return os.path.join(config_folder, "vocab_builder.db")
 
 
 def get_vocab_builder_db(addon_path: str) -> VocabBuilderDB:
-    return VocabBuilderDB(get_db_path(addon_path))
+    return VocabBuilderDB(get_db_path())
