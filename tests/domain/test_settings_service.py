f91c0c6d114d265e5ceb66690c6a295fe27b87b9
Joey <mycityofsky@gmail.com>
2022-05-02 12:01:10 +0800

    pass test_should_update_settings_when_there_is_old_record_before

--- tests/domain/test_settings_service.py
+++ tests/domain/test_settings_service.py
@@ -13,8 +13,16 @@ class SettingsServiceTestCase(unittest.TestCase):
 
     def test_should_update_settings_when_there_is_no_record_before(self):
         settings = Settings(True, 20, "/test/path")
-
         self.__settings_service.update_settings(settings)
 
         obtained_settings = self.__settings_service.get_settings()
         self.assertEqual(settings, obtained_settings)
+
+    def test_should_update_settings_when_there_is_old_record_before(self):
+        self.__settings_service.update_settings(Settings(True, 20, "/test/path"))
+
+        new_settings = Settings(False, 0, "")
+        self.__settings_service.update_settings(Settings(False, 0, ""))
+
+        obtained_settings = self.__settings_service.get_settings()
+        self.assertEqual(new_settings, obtained_settings)
