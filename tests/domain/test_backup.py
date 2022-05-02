64bdfd527abd7f78e5579c337b9e406a3b7ad937
Joey <mycityofsky@gmail.com>
2022-05-02 22:31:44 +0800

    pass test_get_backup_time

--- tests/domain/test_backup.py
+++ tests/domain/test_backup.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from datetime import datetime
 
 from vocab_builder.domain.backup.Backup import Backup
 
@@ -7,9 +8,11 @@ from vocab_builder.domain.backup.Backup import Backup
 class BackupTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.backup_name = "anki_vocab_builder_backup_2022050111003.db"
+        self.backup_name = "anki_vocab_builder_backup_20220501110003.db"
         self.backup = Backup(os.path.join("/tmp", self.backup_name))
 
     def test_get_backup_name(self):
         self.assertEqual(self.backup.get_backup_name(), self.backup_name)
 
+    def test_get_backup_time(self):
+        self.assertEqual(self.backup.get_backup_time(), datetime(2022, 5, 1, 11, 0, 3))
--- vocab_builder/domain/backup/Backup.py
+++ vocab_builder/domain/backup/Backup.py
@@ -1,14 +1,22 @@
+from datetime import datetime
 from pathlib import Path
 
 class Backup:
 
     def __init__(self, backup_path: str):
         self.backup_path = backup_path
+        self.name_prefix = "anki_vocab_builder_backup_"
+        self.name_suffix = ".db"
 
     def get_backup_name(self) -> str:
         return Path(self.backup_path).name
 
+    def get_backup_time(self) -> datetime:
+        """Get backup time from backup name"""
+        date_time_str = self.get_backup_name()[len(self.name_prefix):-len(self.name_suffix)]
+        return datetime.strptime(date_time_str, "%Y%m%d%H%M%S")
+
     def __eq__(self, other):
         if isinstance(other, Backup):
-            return self.backup_name == other.backup_name and self.backup_path == other.backup_path
+            return self.backup_path == other.backup_path
         return False
