0c7066eb84b99bab3b25b103e44227329375d59b
Joey <mycityofsky@gmail.com>
2022-05-02 08:01:49 +0800

    update delimiters

--- vocab_builder/domain/utils/StringUtils.py
+++ vocab_builder/domain/utils/StringUtils.py
@@ -15,21 +15,22 @@ def split(delimiters: Tuple[str, ...], text: str) -> [str]:
     return re.split(regex_pattern, text)
 
 
-def split_with_positions(delimiters: Tuple[str, ...], text: str) -> [(str, int)]:
+def split_with_positions(delimiters: [str], text: str) -> [(str, int)]:
     """Split text according to delimiters, returns each word and its start position after splitting.
     Empty strings are excluded
 
     Parameters:
-        delimiters: A tuple of strings, the length of each one shuold only be one
+        delimiters: A list of strings, the length of each one shuold only be one
+        text: The text to split
 
     Returns:
         A list of tuples, each tuple contains:
             1. A word
             2. The starting position of the word
 
-    >>> split_with_positions(("a", "b", "c"), "XXXaYYYbZZZcDDD")
+    >>> split_with_positions(["a", "b", "c"], "XXXaYYYbZZZcDDD")
     [('XXX', 0), ('YYY', 4), ('ZZZ', 8), ('DDD', 12)]
-    >>> split_with_positions(("a", "b", "c"), "XXXaYYYbc")
+    >>> split_with_positions(["a", "b", "c"], "XXXaYYYbc")
     [('XXX', 0), ('YYY', 4)]
     """
 
