e94ba10b4793ee4cfe39a3bfe681e1d2f2868066
Joey <mycityofsky@gmail.com>
2022-05-02 10:45:52 +0800

    remove the anki-addon-folder parameter

--- vocab_builder/ui/util/DatabaseUtils.py
+++ vocab_builder/ui/util/DatabaseUtils.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 from vocab_builder.infrastructure import get_vocab_builder_db, VocabBuilderDB
-from vocab_builder.ui.util.AnkiUtils import get_addon_dir
 
 
 db: Optional[VocabBuilderDB] = None
@@ -11,5 +10,5 @@ def get_prod_vocab_builder_db() -> VocabBuilderDB:
     global db
     if db is not None:
         return db
-    db = get_vocab_builder_db(get_addon_dir())
+    db = get_vocab_builder_db()
     return db
