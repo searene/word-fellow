af35da62b63320ea50daed5e457566120d742738
Joey <mycityofsky@gmail.com>
2022-05-05 20:59:36 +0800

    remove test db

--- tests/ui/test_backup_detail_dialog.py
+++ tests/ui/test_backup_detail_dialog.py
@@ -1,3 +1,4 @@
+import os.path
 import sys
 import unittest
 
@@ -6,9 +7,9 @@ from PyQt5.QtTest import QTest
 from PyQt5.QtWidgets import QApplication
 
 from tests.utils import get_test_vocab_builder_db, get_test_db_path
-from vocab_builder.domain.backup.Backup import Backup
 from vocab_builder.domain.backup.BackupService import BackupService
 from vocab_builder.domain.settings.SettingsService import SettingsService
+from vocab_builder.domain.utils import FileUtils
 from vocab_builder.ui.dialog.settings.backup.BackupDetailDialog import BackupDetailDialog
 
 
@@ -19,12 +20,15 @@ class BackupDetailDialogTestCase(unittest.TestCase):
         cls.__app = QApplication(sys.argv)
 
     def setUp(self):
-        db = get_test_vocab_builder_db()
-        settings_service = SettingsService(db)
-        self.__backup_service = BackupService(settings_service, db_path=db.db_path)
+        self.__db = get_test_vocab_builder_db()
+        settings_service = SettingsService(self.__db)
+        self.__backup_service = BackupService(settings_service, db_path=self.__db.db_path)
 
         self.__backup = self.__backup_service.run_backup(force_run=True)
         self.__form = BackupDetailDialog(None, self.__backup, self.__backup_service, False)
+
+    def tearDown(self) -> None:
+        os.remove(self.__db.db_path)
 
     def test_restore(self) -> None:
 
@@ -38,7 +42,7 @@ class BackupDetailDialogTestCase(unittest.TestCase):
         # verify
         new_db = get_test_vocab_builder_db()
         new_settings_service = SettingsService(new_db)
-        new_backup_service = BackupService(new_settings_service, db_path=get_test_db_path())
+        new_backup_service = BackupService(new_settings_service, db_path=self.__db.db_path)
         new_backup_config = new_backup_service.get_backup_config()
         self.assertEqual(new_backup_config, backup_config)
 
