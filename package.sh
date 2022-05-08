9fc7eb01eeb9442ef1ce400fcd9e7728a318ab91
Joey <mycityofsky@gmail.com>
2022-05-08 13:29:38 +0800

    make package.sh work

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@ TARGET_DIR=/tmp/word_fellow
 rm -rf ${TARGET_DIR}
 mkdir ${TARGET_DIR}
 cp -r word_fellow/* ${TARGET_DIR}
+echo "\n\n\ninit_addon()\n" >> ${TARGET_DIR}/__init__.py
 pushd ${TARGET_DIR} || exit
 zip -r ../word_fellow.ankiaddon *
 rm -rf ${TARGET_DIR:?}/*
--- word_fellow/__init__.py
+++ word_fellow/__init__.py
@@ -1,4 +1,1 @@
 from word_fellow.ui import init_addon
-
-if __name__ == "__main__":
-    init_addon()
