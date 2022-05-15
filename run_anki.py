0e30aa4879c48d8ee635535f61b2d62d2a6a391c
Joey <mycityofsky@gmail.com>
2022-05-15 11:52:18 +0800

    let __init__.py be usable both when we are under tests and anki is running

--- run_anki.py
+++ run_anki.py
@@ -2,7 +2,6 @@ from aqt import _run
 from word_fellow.ui import init_addon
 
 # Initialize Anki
-
 app = _run(exec=False)
 
 # Initialize our addon
--- word_fellow/__init__.py
+++ word_fellow/__init__.py
@@ -1,1 +1,10 @@
+import sys
+
+import aqt
+
 from word_fellow.ui import init_addon
+
+
+if 'unittest' not in sys.modules.keys() and aqt.mw is not None:
+    # Only initialize the addon when the addon is not being tested and Anki is running
+    init_addon()
