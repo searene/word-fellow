3086adf4d9701e75c4f17efe6bb50ead02d99c65
Joey <mycityofsky@gmail.com>
2022-05-19 22:31:35 +0800

    use numbers

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 # This script is used to deploy the add-on to Anki
 
 PACKAGE_DIR=/tmp/word_fellow
-ANKI_ADDON_DIR=$HOME/Library/Application\ Support/Anki2/addons21
+ANKI_ADDON_DIR=$HOME/Library/Application\ Support/Anki2/addons21/1249872340
 
 ./package.sh "${PACKAGE_DIR}"
 
-rm -rf "${ANKI_ADDON_DIR}/word_fellow"
-unzip "${PACKAGE_DIR}/word_fellow.ankiaddon" -d "${ANKI_ADDON_DIR}/1249872340"
+rm -rf "${ANKI_ADDON_DIR}"
+unzip "${PACKAGE_DIR}/word_fellow.ankiaddon" -d "${ANKI_ADDON_DIR}"
