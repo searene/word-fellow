aa01a2fbad464777eba1e03f924db1a551458e2e
Joey <mycityofsky@gmail.com>
2022-05-20 19:48:16 +0800

    change target addon folder

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # This script is used to deploy the add-on to Anki
 
 PACKAGE_DIR=/tmp/word_fellow
-ANKI_ADDON_DIR=$HOME/Library/Application\ Support/Anki2/addons21/1249872340
+ANKI_ADDON_DIR=$HOME/Library/Application\ Support/Anki2/addons21/word_fellow
 
 ./package.sh "${PACKAGE_DIR}"
 
